6a99944224d6b5cd9b3a5081f0a7cef430ec84f2
zouzhaoqian <zouzhaoqian@yeah.net>
2019-11-29 19:47:44 +0800

    测试连接2

--- src/main/java/com/zou/SpringBootJunitApplication.java
+++ src/main/java/com/zou/SpringBootJunitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootJunitApplication {
     public static void main(String[] args) {
+        System.out.println("项目启动");
         SpringApplication.run(SpringBootJunitApplication.class);
     }
 }
